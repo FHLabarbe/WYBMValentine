ee1c92be27bbd3fc8183ebb907430cc0177a09a7
FHLabarbe <fh.labarbe@gmail.com>
2026-02-02 00:03:19 +0100

    fix: corrections images sur deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://FHLabarbe.github.io",
+  site: "https://fhlabarbe.github.io",
   base: "/WYBMValentine",
 });
